88d92eeea9160d2eef6ba4eec8f946d5e3f9f7ff
Kaoru <kaoru@gmail.com>
2024-09-02 12:51:46 +0800

    errors除錯

--- user/urls.py
+++ user/urls.py
@@ -3,4 +3,5 @@ from . import views
 
 urlpatterns = [
     path("user-register/", views.user_register, name="user-register"),
+    path("user_login/", views.user_login, name="user-login"),
 ]
--- user/views.py
+++ user/views.py
@@ -1,6 +1,8 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
 from django.contrib.auth.models import User
 from django.contrib.auth.forms import UserCreationForm
+from django.contrib.auth import login, logout
+from django.contrib.auth import authenticate
 
 # Create your views here.
 
@@ -25,3 +27,22 @@ def user_register(request):
     return render(
         request, "user/user-register.html", {"message": message, "form": form}
     )
+
+
+# 登入，直接除理表單(抓取欄位)
+def user_login(request):
+    message = ""
+
+    if request.method == "POST":
+        username = request.POST.get("username")
+        password = request.POST.get("password")
+        user = authenticate(request, username=username, password=password)
+
+        if user:
+            login(request, user)
+            message = "登入成功"
+
+        else:
+            message = "資料錯誤"
+
+    return render(request, "user-login.html", {"message": message})
