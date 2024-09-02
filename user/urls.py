1adb216cc934de2d79920b696667545780bda116
Kaoru <kaoru@gmail.com>
2024-09-02 11:10:35 +0800

    完成base.html

--- user/urls.py
+++ user/urls.py
@@ -1,3 +1,6 @@
 from django.urls import path
+from . import views
 
-urlpatterns = []
+urlpatterns = [
+    path("user-register", views.user_register, name="user-register"),
+]
--- user/views.py
+++ user/views.py
@@ -1,3 +1,26 @@
 from django.shortcuts import render
+from django.contrib.auth.models import User
+from django.contrib.auth.forms import UserCreationForm
 
 # Create your views here.
+
+
+# 註冊(使用內建UserCreationForm表單)
+def user_register(request):
+    message = ""
+    if request.method == "POST":
+        form = UserCreationForm(request.POST)
+
+        # 會自動驗證表單內容，若無誤就建立表單
+        if form.is_valid():
+            form.save()
+
+        else:
+            message = "資料錯誤:"
+
+    else:
+        form = UserCreationForm()
+
+    return render(
+        request, "user/user-register.html", {"message": message, "form": form}
+    )
